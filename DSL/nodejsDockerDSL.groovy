749341efc82548914697ffa68d0416f88fce23e2
Daniel Vinagre Luengo <60425460+Da-viel@users.noreply.github.com>
2022-12-13 20:57:02 +0100

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('Da-viel/nodejsapp')
+            repositoryName('da-viel/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
